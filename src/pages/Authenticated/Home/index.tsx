757b29da974b58131c175c08698d1b5b498abe5d
Yago Juan <yago.juan@teclat.com.br>
2021-08-12 00:40:48 -0300

    Adiciona o header na tela Home

--- src/pages/Authenticated/Home/index.tsx
+++ src/pages/Authenticated/Home/index.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import {
+  Card,
+  Container,
+  Header,
+  SubTitle,
+  Title,
+  TitleTexts,
+  TopHeader,
+  TotalValue,
+  TotalValueTextGreen,
+  Value,
+  Values,
+  ValueSubTitleGreen,
+  ValueSubTitleRed,
+  ValueTitleGreen,
+  ValueTitleRed,
+} from "./styles";
 
 export default function Home() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+  return (
+    <Container>
+      <Header>
+        <TopHeader>
+          <TitleTexts>
+            <Title>Saldo atual</Title>
+            <SubTitle>R$ 1.000,00</SubTitle>
+          </TitleTexts>
+        </TopHeader>
+
+        <Card>
+          <Values>
+            <Value>
+              <ValueTitleGreen>Entradas</ValueTitleGreen>
+              <ValueSubTitleGreen>R$ 1.200,00</ValueSubTitleGreen>
+            </Value>
+            <Value>
+              <ValueTitleRed>Saídas</ValueTitleRed>
+              <ValueSubTitleRed>R$ 1.100,00</ValueSubTitleRed>
+            </Value>
+          </Values>
+
+          <TotalValue>
+            <TotalValueTextGreen>
+              Você tem R$ 100,00 de lucro
+            </TotalValueTextGreen>
+          </TotalValue>
+        </Card>
+      </Header>
+    </Container>
+  );
 }
